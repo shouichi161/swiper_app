42d489a2d8b0567b52cacc66bfaacd1494f07920
EC2 Default User <124329029+shouichi161@users.noreply.github.com>
2023-08-14 12:43:17 +0000

    [add]aboutページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'homes/top' => 'homes#top'
+  get 'homes/about' => 'homes#about',as:'about'
   devise_for :users
   root to:'tests#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
