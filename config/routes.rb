3c648715b0aef7ffdb6d12224f743e514adc2e1d
EC2 Default User <124329029+shouichi161@users.noreply.github.com>
2023-08-17 11:35:18 +0000

    [fix]post_image controller削除、post_images controllre作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  resources :post_images,only: [:new,:create,:index,:show]
-  get 'homes/top' => 'homes#top'
-  get 'homes/about' => 'homes#about',as:'about'
   devise_for :users
   root to:'tests#top'
+
+  resources :post_images, only: [:new, :create, :index, :show]
+  get 'homes/top' => 'homes#top'
+  get 'homes/about' => 'homes#about',as:'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
