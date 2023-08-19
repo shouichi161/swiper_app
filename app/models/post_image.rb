fa4c00b30ce5796bdf7ac5de5d72b597b7dcc06c
EC2 Default User <124329029+shouichi161@users.noreply.github.com>
2023-08-19 13:06:39 +0000

    [add]いいね機能を追加

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,9 +1,14 @@
 class PostImage < ApplicationRecord
   belongs_to :user
   has_many :post_comments,dependent: :destroy
+  has_many :favorites,dependent: :destroy
 
 
   has_one_attached :image
+
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
 
 
   def get_image
