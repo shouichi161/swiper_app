0bff8eda722c74ea240d26afec0570891f0b9546
EC2 Default User <124329029+shouichi161@users.noreply.github.com>
2023-08-20 13:22:43 +0000

    [add]post_imageの新規投稿にバリデーションを追加

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -3,6 +3,8 @@ class PostImage < ApplicationRecord
   has_many :post_comments,dependent: :destroy
   has_many :favorites,dependent: :destroy
 
+  validates :shop_name,presence: true
+  validates :image,presence: true
 
   has_one_attached :image
 
